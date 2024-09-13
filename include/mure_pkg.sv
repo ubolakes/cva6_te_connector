72324cdb41f4288c5e5a7b360dc30e3b02b52387
Umberto Laghi <ul2509@gmail.com>
2024-09-13 18:10:35 +0200

    added mask to detect itype

--- include/mure_pkg.sv
+++ include/mure_pkg.sv
@@ -7,7 +7,7 @@ package mure_pkg;
     localparam PRIV_LEN = 2; // depends on CPU implementation
     localparam INST_LEN = 32;
     localparam ILASTSIZE_LEN = 2;
-    localparam ITYPE_LEN = 4;
+    localparam ITYPE_LEN = 3;
 `ifdef TRDB_ARCH64 // 64bit arch specific parameters
     localparam XLEN = 64;
 `else // 32bit arch
@@ -43,7 +43,7 @@ typedef enum logic[ITYPE_LEN-1:0] {
     UJ = 6, // uninferable jump if ITYPE_LEN == 3, otherwise reserved
     RES = 7, // reserved
     UC = 8, // uninferable call
-    IC = 9, // inferrable call
+    IC = 9, // inferable call
     UJ = 10, // uninferable jump
     IJ = 11, // inferable jump
     CRS = 12, // co-routine swap
@@ -51,5 +51,45 @@ typedef enum logic[ITYPE_LEN-1:0] {
     OUJ = 14, // other uninferable jump
     OIJ = 15 // other inferable jump
 } itype_e;
+
+/* mask and match parameter for itype determination */
+parameter MASK_BEQ = 32'h707f;
+parameter MATCH_BEQ = 32'h63;
+parameter MASK_BNE = 32'h707f;
+parameter MATCH_BNE = 32'h1063;
+parameter MASK_BLT = 32'h707f;
+parameter MATCH_BLT = 32'h4063;
+parameter MASK_BGE = 32'h707f;
+parameter MATCH_BGE = 32'h5063;
+parameter MASK_BLTU = 32'h707f;
+parameter MATCH_BLTU = 32'h6063;
+parameter MASK_BGEU = 32'h707f;
+parameter MATCH_BGEU = 32'h7063;
+parameter MASK_P_BNEIMM = 32'h707f;
+parameter MATCH_P_BNEIMM = 32'h3063;
+parameter MASK_P_BEQIMM = 32'h707f;
+parameter MATCH_P_BEQIMM = 32'h2063;
+parameter MASK_C_BEQZ = 32'he003;
+parameter MATCH_C_BEQZ = 32'hc001;
+parameter MASK_C_BNEZ = 32'he003;
+parameter MATCH_C_BNEZ = 32'he001;
+parameter MASK_C_JALR = 32'hf07f;
+parameter MATCH_C_JALR = 32'h9002;
+parameter MASK_RD = 32'hf80;
+parameter MASK_C_JR = 32'hf07f;
+parameter MATCH_C_JR = 32'h8002;
+parameter MASK_JALR = 32'h707f;
+parameter MATCH_JALR = 32'h67;
+parameter MASK_MRET = 32'hffffffff;
+parameter MATCH_MRET = 32'h30200073;
+parameter MASK_SRET = 32'hffffffff;
+parameter MATCH_SRET = 32'h10200073;
+parameter MASK_URET = 32'hffffffff;
+parameter MATCH_URET = 32'h200073;
+parameter MASK_RS1 = 32'hf8000;
+parameter MASK_IMM = 32'hfff00000;
+parameter X_RA = 32'h1;
+parameter OP_SH_RS1 = 32'd15;
+parameter OP_SH_RD = 32'd7;
 
 endpackage
