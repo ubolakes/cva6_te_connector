756c0f539362249c378d5ccebcc8e16fac178655
Umberto Laghi <ul2509@gmail.com>
2024-09-16 14:48:02 +0200

    made instLast a register

--- rtl/ingress_fsm.sv
+++ rtl/ingress_fsm.sv
@@ -23,6 +23,7 @@ module ingress_fsm (
 logic single_ret;
 logic multiple_ret;
 logic all_types_0;
+uop_entry_s instLast_d, instLast_q;
 
 assign single_ret = (ivalids_i != 0) && ((ivalids_i & (ivalids_i - 1)) == 0);
 assign all_types_0 =    uop_a_i.itype == STD &&
@@ -30,6 +31,7 @@ assign all_types_0 =    uop_a_i.itype == STD &&
                         uop_c_i.itype == STD &&
                         uop_d_i.itype == STD;
 assign multiple_ret = $countones(ivalids_i) > 1 && all_types_0;
+assign instLast_o = instLast_q;
 
 // states definition
 typedef enum logic [2:0] { 
@@ -147,8 +149,10 @@ end
 always_ff @(posedge clk_i, negedge rst_ni) begin
     if (!rst_ni) begin
         current_state <= WAIT_A;
+        instLast_q <= '0;
     end else begin
         current_state <= next_state;
+        instLast_q <= instLast_d;
     end
 end
 
