ca9ca96ce9907d77cfd2493f45e8b94ea5e0a884
UMBERTO LAGHI <ul2509@gmail.com>
2024-09-27 12:32:26 +0200

    edited signals width, removed synchronous logic

--- rtl/multiple_retirement.sv
+++ rtl/multiple_retirement.sv
@@ -5,7 +5,7 @@
 /* TOP LEVEL MODULE */
 
 module multiple_retirement #(
-    parameter int unsigned NrRetiredInstr = 2; // placeholder value, CVA6
+    parameter int unsigned NrRetiredInstr = 2 // placeholder value, CVA6
 )
 (
     input logic clk_i,
@@ -13,8 +13,8 @@ module multiple_retirement #(
 
     /* data from the CPU */
     // inputs
-    input logic [NrRetiredInstr-1:0]/*[?:?]*/                   iretire_i,
-    input logic [NrRetiredInstr-1:0]/*[?:?]*/                   ilastsize_i, // TODO: define correct size
+    input logic [NrRetiredInstr-1:0][0:0]                       iretire_i,
+    input logic [NrRetiredInstr-1:0][mure_pkg::ILASTSIZE_LEN-1:0]   ilastsize_i, // TODO: define correct size
     input logic [NrRetiredInstr-1:0][mure_pkg::ITYPE_LEN-1:0]   itype_i,
     input logic [mure_pkg::CAUSE_LEN-1:0]                       cause_i,
     input logic [mure_pkg::XLEN-1:0]                        tval_i,
@@ -27,11 +27,11 @@ module multiple_retirement #(
 
     // outputs
     /* the output of the module goes directly into the trace_encoder module */
-    output logic                                                iretire_o,
-    output logic                                                ilastsize_o, // TODO: define correct size
+    output logic [0:0]                                          iretire_o,
+    output logic [mure_pkg::ILASTSIZE_LEN-1:0]                  ilastsize_o, // TODO: define correct size
     output logic [mure_pkg::ITYPE_LEN-1:0]                      itype_o,
     output logic [mure_pkg::CAUSE_LEN-1:0]                      cause_o,
-    output logic [mure_pkg::XLEN-1:0]                       tval_o,
+    output logic [mure_pkg::XLEN-1:0]                           tval_o,
     output logic [mure_pkg::PRIV_LEN-1:0]                       priv_o,
     output logic [mure_pkg::XLEN-1:0]                           iaddr_o
     //output logic [mure_pkg::CTX_LEN-1:0]                        context_o, // non mandatory
@@ -41,8 +41,8 @@ module multiple_retirement #(
 );
 
 // entries for the FIFOs
-uop_entry_s                         uop_entry_i[NrRetiredInstr], uop_entry_o[NrRetiredInstr];
-common_entry_s                      common_entry_i, common_entry_o;
+mure_pkg::uop_entry_s [NrRetiredInstr-1:0]  uop_entry_i, uop_entry_o;
+mure_pkg::common_entry_s                    common_entry_i, common_entry_o;
 // FIFOs management
 logic                               pop; // signal to pop FIFOs
 logic                               empty[NrRetiredInstr]; // signal used to enable counter
@@ -53,10 +53,9 @@ logic [$clog2(NrRetiredInstr)-1:0]  cnt_val;
 logic                               clear_counter;
 logic                               enable_counter;
 
-
 // assignments
 assign pop = cnt_val == NrRetiredInstr-1;
-assign push_enable = !full[0];// maybe: at least one instr committed?
+assign push_enable = !full[0]; // maybe: at least one instr committed?
 assign clear_counter = cnt_val == NrRetiredInstr-1;
 assign enable_counter = !empty[0]; // the counter goes on if FIFOs are not empty
 
@@ -75,7 +74,7 @@ for (genvar i = 0; i < NrRetiredInstr; i++) begin
         .empty_o(empty[i]),
         .usage_o(),
         .data_i(uop_entry_i[i]),
-        .push_i(),
+        .push_i(push_enable),
         .data_o(uop_entry_o[i]),
         .pop_i(pop)
     );
@@ -88,15 +87,15 @@ fifo_v3 #(
 ) ingressFIFO_cmn (
     .clk_i  (clk_i),
     .rst_ni (rst_ni),
-    .flush_i(),
-    .testmode_i(),
+    .flush_i('0),
+    .testmode_i('0),
     .full_o(),
     .empty_o(),
     .usage_o(),
     .data_i(common_entry_i),
-    .push_i(),
+    .push_i(push_enable),
     .data_o(common_entry_o),
-    .pop_i()
+    .pop_i(pop)
 );
 
 // counter instantation - from common_cells
@@ -115,9 +114,7 @@ counter #(
     .overflow_o()
 );
 
-// does it make sense to put this logic in a module?
-// combinatorial logic to manage the MUX
-always_comb begin : multiplexer
+always_comb begin
     // initialization
     iretire_o = '0;
     ilastsize_o = '0;
@@ -127,6 +124,18 @@ always_comb begin : multiplexer
     priv_o = '0;
     iaddr_o = '0;
 
+    // populating uop FIFO entry
+    for (int i = 0; i < NrRetiredInstr; i++) begin
+        uop_entry_i[i].itype = itype_i[i];
+        uop_entry_i[i].iaddr = iaddr_i[i];
+        uop_entry_i[i].iretire = iretire_i[i];
+        uop_entry_i[i].ilastsize = ilastsize_i[i];
+    end
+    // populating common FIFO entry
+    common_entry_i.cause = cause_i;
+    common_entry_i.tval = tval_i;
+    common_entry_i.priv = priv_i;
+
     // determinating output
     iretire_o = uop_entry_o[cnt_val].iretire;
     ilastsize_o = uop_entry_o[cnt_val].ilastsize;
@@ -135,30 +144,6 @@ always_comb begin : multiplexer
     cause_o = common_entry_o.cause;
     tval_o = common_entry_o.tval;
     priv_o = common_entry_o.priv;
-
-end
-
-/* REGISTERS MANAGEMENT */
-always_ff @( posedge clk_i, negedge rst_ni ) begin
-    if(~rst_ni) begin
-        
-    end else if (push_enable) begin
-        // populating uop FIFO
-        for (int i = 0; i < NrRetiredInstr; i++) begin
-            uop_entry_i[i].itype = itype_i[i];
-            uop_entry_i[i].iaddr = iaddr_i[i];
-            uop_entry_i[i].iretire = iretire_i[i];
-            uop_entry_i[i].ilastsize = ilastsize_i[i];
-        end
-        // populating common FIFO
-        common_entry_i.cause = cause_i;
-        common_entry_i.tval = tval_i;
-        common_entry_i.priv = priv_i;
-        //common_entry_i.context = context_i; // non mandatory
-        //common_entry_i.ctype = ctype_i; // non mandatory
-        //common_entry_i.time = time_i; // non mandatory
-        //common_entry_i.sijump = sijump_i; // non mandatory
-    end
 end
 
 endmodule
