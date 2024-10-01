d6864d1d52dd5156a7b1efb9b23817e849caabbf
Umberto Laghi <ul2509@gmail.com>
2024-10-01 08:37:14 +0200

    edited fsm logic

--- rtl/fsm.sv
+++ rtl/fsm.sv
@@ -29,6 +29,9 @@ logic [mure_pkg::XLEN-1:0]      iaddr_d, iaddr_q;
 logic [mure_pkg::XLEN-1:0]      iretire_d, iretire_q;
 logic                           ilastsize_d, ilastsize_q;
 logic [mure_pkg::ITYPE_LEN-1:0] itype_d, itype_q;
+logic [mure_pkg::CAUSE_LEN-1:0] cause_d, cause_q;
+logic [mure_pkg::XLEN-1:0]      tval_d, tval_q;
+logic [mure_pkg::PRIV_LEN-1:0]  priv_d, priv_q;
 logic                           exception;
 logic                           special_inst;
 logic                           valid_d, valid_q;
@@ -40,9 +43,9 @@ assign special_inst = fifo_entry_i.itype > 1 && fifo_entry_i.valid || exception;
 assign iretire_o = iretire_q;
 assign ilastsize_o = ilastsize_q;
 assign itype_o = itype_q;
-assign cause_o = fifo_entry_i.cause;
-assign tval_o = fifo_entry_i.tval;
-assign priv_o = fifo_entry_i.priv;
+assign cause_o = cause_q;
+assign tval_o = tval_q;
+assign priv_o = priv_q;
 assign iaddr_o = iaddr_q;
 assign valid_o = valid_q;
 assign rst_sync_d = valid_q;
@@ -56,6 +59,9 @@ always_comb begin
     iretire_d = '0;
     ilastsize_d = '0;
     itype_d = '0;
+    cause_d = '0;
+    tval_d = '0;
+    priv_d = '0;
 
     case (current_state)
     mure_pkg::IDLE: begin
@@ -71,10 +77,13 @@ always_comb begin
             iretire_d = fifo_entry_i.compressed ? 1 : 2;
             ilastsize_d = !fifo_entry_i.compressed;
             itype_d = fifo_entry_i.itype;
-            // remains here
-            next_state = mure_pkg::IDLE;
+            cause_d = fifo_entry_i.cause;
+            tval_d = fifo_entry_i.tval;
+            priv_d = fifo_entry_i.priv;
             // output readable
             valid_d = '1;
+            // remains here
+            next_state = mure_pkg::IDLE;
         end
     end
 
@@ -89,12 +98,15 @@ always_comb begin
             iretire_d = fifo_entry_i.compressed ? iretire_q + 1 : iretire_q + 2;
             // sets ilastsize
             ilastsize_d = !fifo_entry_i.compressed;
-            // sets itype
+            // sets all params for output
             itype_d = fifo_entry_i.itype;
-            // goes to IDLE
-            next_state = mure_pkg::IDLE;
+            cause_d = fifo_entry_i.cause;
+            tval_d = fifo_entry_i.tval;
+            priv_d = fifo_entry_i.priv;
             // output readable
             valid_d = '1;
+            // goes to IDLE
+            next_state = mure_pkg::IDLE;
         end
     end
 
@@ -109,6 +121,9 @@ always_ff @(posedge clk_i, negedge rst_ni) begin
         iretire_q <= '0;
         ilastsize_q <= '0;
         itype_q <= '0;
+        cause_q <= '0;
+        tval_q <= '0;
+        priv_q <= '0;
         valid_q <= '0;
         rst_sync_q <= '0;
     end else if (rst_sync_q) begin
@@ -116,12 +131,18 @@ always_ff @(posedge clk_i, negedge rst_ni) begin
         iretire_q <= '0;
         ilastsize_q <= '0;
         itype_q <= '0;
+        cause_q <= '0;
+        tval_q <= '0;
+        priv_q <= '0;
     end else begin
         current_state <= next_state;
         iaddr_q <= iaddr_d;
         iretire_q <= iretire_d;
         ilastsize_q <= ilastsize_d;
         itype_q <= itype_d;
+        cause_q <= cause_d;
+        tval_q <= tval_d;
+        priv_q <= priv_d;
         valid_q <= valid_d;
         rst_sync_q <= rst_sync_d;
     end
