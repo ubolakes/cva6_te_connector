7c2941a20e55f440528e5a71e015dba6321f641c
UMBERTO LAGHI <ul2509@gmail.com>
2024-10-01 10:26:20 +0200

    edited logic to make it work as expected

--- rtl/fsm.sv
+++ rtl/fsm.sv
@@ -27,47 +27,41 @@ module fsm (
 mure_pkg::state_e current_state, next_state;
 logic [mure_pkg::XLEN-1:0]      iaddr_d, iaddr_q;
 logic [mure_pkg::XLEN-1:0]      iretire_d, iretire_q;
-logic                           ilastsize_d, ilastsize_q;
-logic [mure_pkg::ITYPE_LEN-1:0] itype_d, itype_q;
-logic [mure_pkg::CAUSE_LEN-1:0] cause_d, cause_q;
-logic [mure_pkg::XLEN-1:0]      tval_d, tval_q;
-logic [mure_pkg::PRIV_LEN-1:0]  priv_d, priv_q;
 logic                           exception;
 logic                           special_inst;
-logic                           valid_d, valid_q;
-logic                           rst_sync_d, rst_sync_q;
+logic                           one_cycle;
+logic                           more_cycles;
+logic                           update_iaddr;
 
 /* assignments */
 assign exception = fifo_entry_i.itype == 1;
 assign special_inst = fifo_entry_i.itype > 1 && fifo_entry_i.valid || exception;
-assign iretire_o = iretire_q;
-assign ilastsize_o = ilastsize_q;
-assign itype_o = itype_q;
-assign cause_o = cause_q;
-assign tval_o = tval_q;
-assign priv_o = priv_q;
-assign iaddr_o = iaddr_q;
-assign valid_o = valid_q;
-assign rst_sync_d = valid_q;
+assign iretire_o = (one_cycle || more_cycles) ? iretire_d : iretire_q;
+assign iaddr_o = one_cycle ? iaddr_d : iaddr_q;
 
 // combinatorial logic for state transition
 always_comb begin
     // next_state default value
     next_state = current_state;
     // init
-    valid_d = '0;
+    valid_o = '0;
     iretire_d = '0;
-    ilastsize_d = '0;
-    itype_d = '0;
-    cause_d = '0;
-    tval_d = '0;
-    priv_d = '0;
+    ilastsize_o = '0;
+    itype_o = '0;
+    cause_o = '0;
+    tval_o = '0;
+    priv_o = '0;
+    iaddr_d = '0;
+    one_cycle = '0;
+    more_cycles = '0;
+    update_iaddr = '0;
 
     case (current_state)
     mure_pkg::IDLE: begin
         if (fifo_entry_i.itype == 0 && fifo_entry_i.valid) begin // standard instr and valid
             // sets iaddr, increases iretire
             iaddr_d = fifo_entry_i.pc;
+            update_iaddr = '1;
             iretire_d = fifo_entry_i.compressed ? iretire_q + 1 : iretire_q + 2;
             // goes to COUNT
             next_state = mure_pkg::COUNT;
@@ -75,13 +69,15 @@ always_comb begin
             // set all params for output
             iaddr_d = fifo_entry_i.pc;
             iretire_d = fifo_entry_i.compressed ? 1 : 2;
-            ilastsize_d = !fifo_entry_i.compressed;
-            itype_d = fifo_entry_i.itype;
-            cause_d = fifo_entry_i.cause;
-            tval_d = fifo_entry_i.tval;
-            priv_d = fifo_entry_i.priv;
+            ilastsize_o = !fifo_entry_i.compressed;
+            itype_o = fifo_entry_i.itype;
+            cause_o = fifo_entry_i.cause;
+            tval_o = fifo_entry_i.tval;
+            priv_o = fifo_entry_i.priv;
             // output readable
-            valid_d = '1;
+            valid_o = '1;
+            // read now the output
+            one_cycle = '1;
             // remains here
             next_state = mure_pkg::IDLE;
         end else begin
@@ -99,14 +95,16 @@ always_comb begin
             // increases iretire
             iretire_d = fifo_entry_i.compressed ? iretire_q + 1 : iretire_q + 2;
             // sets ilastsize
-            ilastsize_d = !fifo_entry_i.compressed;
+            ilastsize_o = !fifo_entry_i.compressed;
             // sets all params for output
-            itype_d = fifo_entry_i.itype;
-            cause_d = fifo_entry_i.cause;
-            tval_d = fifo_entry_i.tval;
-            priv_d = fifo_entry_i.priv;
+            itype_o = fifo_entry_i.itype;
+            cause_o = fifo_entry_i.cause;
+            tval_o = fifo_entry_i.tval;
+            priv_o = fifo_entry_i.priv;
             // output readable
-            valid_d = '1;
+            valid_o = '1;
+            // read now the output
+            more_cycles = '1;
             // goes to IDLE
             next_state = mure_pkg::IDLE;
         end else begin
@@ -123,34 +121,15 @@ always_ff @(posedge clk_i, negedge rst_ni) begin
         current_state <= mure_pkg::IDLE;
         iaddr_q <= '0;
         iretire_q <= '0;
-        ilastsize_q <= '0;
-        itype_q <= '0;
-        cause_q <= '0;
-        tval_q <= '0;
-        priv_q <= '0;
-        valid_q <= '0;
-        rst_sync_q <= '0;
-    end else if (rst_sync_q) begin
+    end else if (valid_o) begin
         current_state <= next_state;
-        iaddr_q <= '0;
         iretire_q <= '0;
-        ilastsize_q <= '0;
-        itype_q <= '0;
-        cause_q <= '0;
-        tval_q <= '0;
-        priv_q <= '0;
-        valid_q <= valid_d;
+    end else if (update_iaddr) begin
+        current_state <= next_state; 
+        iaddr_q <= iaddr_d;
     end else begin
         current_state <= next_state;
-        iaddr_q <= iaddr_d;
         iretire_q <= iretire_d;
-        ilastsize_q <= ilastsize_d;
-        itype_q <= itype_d;
-        cause_q <= cause_d;
-        tval_q <= tval_d;
-        priv_q <= priv_d;
-        valid_q <= valid_d;
-        rst_sync_q <= rst_sync_d;
     end
 end
 
