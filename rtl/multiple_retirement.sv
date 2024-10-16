d4b94c646ba86368a1aa816afee1195a9b37c271
UMBERTO LAGHI <ul2509@gmail.com>
2024-10-16 16:08:21 +0200

    moved to a new arch

--- rtl/multiple_retirement.sv
+++ rtl/multiple_retirement.sv
@@ -51,26 +51,56 @@ logic                       empty[NRET]; // signal used to enable counter
 logic                       full[NRET];
 logic                       push_enable;
 logic                       at_least_one_valid;
-// counter management
-logic [$clog2(NRET)-1:0]    cnt_val;
-logic                       clear_counter;
-logic                       enable_counter;
+// mux arbiter management
+logic [$clog2(NRET)-1:0]    mux_arb_val;
+logic                       clear_mux_arb;
+logic                       enable_mux_arb;
+// demux arbiter management
+logic [$clog2(N)-1:0]       demux_arb_val;
+logic                       clear_demux_arb;
+logic                       enable_demux_arb;
 // itype_detector
 logic                       is_taken_d, is_taken_q;
 // exception info
 logic [mure_pkg::CAUSE_LEN-1:0] cause_d, cause_q;
 logic [mure_pkg::XLEN-1:0]      tval_d, tval_q;
+// block counter management
+logic                           n_blocks_full;
+logic                           n_blocks_empty;
+logic [$clog2(N):0]             n_blocks_i, n_blocks_o;
+logic                           n_blocks_push;
+logic                           n_blocks_pop;
+// signals to store blocks
+logic                                    valid_fsm;
+logic [N-1:0][mure_pkg::IRETIRE_LEN-1:0] iretire_q;
+logic [N-1:0]                            ilastsize_q;
+logic [N-1:0][mure_pkg::ITYPE_LEN-1:0]   itype_q;
+logic [N-1:0][mure_pkg::CAUSE_LEN-1:0]   cause_q;
+logic [N-1:0][mure_pkg::XLEN-1:0]        tval_q;
+logic [N-1:0][mure_pkg::PRIV_LEN-1:0]    priv_q;
+logic [N-1:0][mure_pkg::XLEN-1:0]        iaddr_q;
+
+logic [mure_pkg::IRETIRE_LEN-1:0] iretire_d;
+logic                             ilastsize_d;
+logic [mure_pkg::ITYPE_LEN-1:0]   itype_d;
+logic [mure_pkg::CAUSE_LEN-1:0]   cause_d;
+logic [mure_pkg::XLEN-1:0]        tval_d;
+logic [mure_pkg::PRIV_LEN-1:0]    priv_d;
+logic [mure_pkg::XLEN-1:0]        iaddr_d;
 
 // assignments
-assign pop = cnt_val == NRET-1;
+assign pop = mux_arb_val == NRET-1;
 assign push_enable = !full[0] && at_least_one_valid;
-assign clear_counter =  cnt_val == NRET-1 ||
+assign clear_mux_arb =  mux_arb_val == NRET-1 ||
                         itype[0] == mure_pkg::EXC ||
                         itype[0] == mure_pkg::INT;
-assign enable_counter = !empty[0]; // the counter goes on if FIFOs are not empty
+assign enable_mux_arb = !empty[0]; // the counter goes on if FIFOs are not empty
 assign is_taken_d = resolved_branch_i.is_taken;
 assign cause_d = exception_i.cause;
 assign tval_d = exception_i.tval;
+assign n_blocks_push = !n_blocks_full;
+assign clear_demux_arb = demux_arb_val == n_blocks_o;
+assign enable_demux_arb = valid_fsm;
 
 /* itype_detectors */
 for (genvar i = 0; i < NRET; i++) begin
@@ -105,65 +135,78 @@ for (genvar i = 0; i < NRET; i++) begin
     );
 end
 
-// counter instantation - from common_cells
+// FIFO to store the n_blocks
+fifo_v3 #(
+    .DEPTH(16),
+    .DATA_WIDTH($clog2(N))
+) i_nblock_fifo (
+    .clk_i     (clk_i),
+    .rst_ni    (rst_ni),
+    .flush_i   ('0),
+    .testmode_i('0),
+    .full_o    (n_blocks_full),
+    .empty_o   (n_blocks_empty),
+    .usage_o   (),
+    .data_i    (n_blocks_i),
+    .push_i    (n_blocks_push),
+    .data_o    (n_blocks_o),
+    .pop_i     (n_blocks_pop)
+);
+
+// mux arbiter for serialization
 counter #(
     .WIDTH($clog2(NRET)),
     .STICKY_OVERFLOW('0)
 ) i_mux_arbiter ( // change name?
     .clk_i     (clk_i),
     .rst_ni    (rst_ni),
-    .clear_i   (clear_counter),
-    .en_i      (enable_counter),
+    .clear_i   (clear_mux_arb),
+    .en_i      (enable_mux_arb),
     .load_i    ('0),
     .down_i    ('0),
     .d_i       ('0),
-    .q_o       (cnt_val),
+    .q_o       (mux_arb_val),
     .overflow_o()
 );
 
-// fsm instantiation
-generate
-    if (N == 1) begin
-        fsm i_fsm (
-            .clk_i      (clk_i),
-            .rst_ni     (rst_ni),
-            .uop_entry_i(uop_entry_mux),
-            .cause_i    (cause_q),
-            .tval_i     (tval_q),
-            .valid_o    (valid_o),
-            .iretire_o  (iretire_o),
-            .ilastsize_o(ilastsize_o),
-            .itype_o    (itype_o),
-            .cause_o    (cause_o),
-            .tval_o     (tval_o),
-            .priv_o     (priv_o),
-            .iaddr_o    (iaddr_o)
-        );
-    end else begin // more special inst per cycle 
-        /*
-        this FSM can output up to N packets in the same cycle
-        and it's possibile because there might be up to N updiscon
-        in the same cycle and they require a specific packet
-        */
-        complex_fsm i_fsm (
-            .clk_i      (clk_i),
-            .rst_ni     (rst_ni),
-            .uop_entry_i(uop_entry_o),
-            .cause_i    (), // wait
-            .tval_i     (),
-            .valid_o    (valid_o),
-            .iretire_o  (iretire_o),
-            .ilastsize_o(ilastsize_o),
-            .itype_o    (itype_o),
-            .cause_o    (cause_o),
-            .tval_o     (tval_o),
-            .priv_o     (priv_o),
-            .iaddr_o    (iaddr_o)
-        );
-    end
-endgenerate
+// fsm to create blocks instantiation
+fsm i_fsm (
+    .clk_i      (clk_i),
+    .rst_ni     (rst_ni),
+    .uop_entry_i(uop_entry_mux),
+    .cause_i    (cause_q),
+    .tval_i     (tval_q),
+    .valid_o    (valid_fsm),
+    .iretire_o  (iretire_d),
+    .ilastsize_o(ilastsize_d),
+    .itype_o    (itype_d),
+    .cause_o    (cause_d),
+    .tval_o     (tval_d),
+    .priv_o     (priv_d),
+    .iaddr_o    (iaddr_d)
+);
+
+// demux arbiter to choose register
+counter #(
+    .WIDTH($clog2(N)),
+    .STICKY_OVERFLOW('0)
+) i_demux_arbiter (
+    .clk_i     (clk_i),
+    .rst_ni    (rst_ni),
+    .clear_i   (clear_demux_arb),
+    .en_i      (enable_demux_arb),
+    .load_i    ('0),
+    .down_i    ('0),
+    .d_i       ('0),
+    .q_o       (demux_arb_val),
+    .overflow_o()
+);
 
 always_comb begin
+    // init
+    n_blocks_i = '0;
+    n_blocks_pop = '0;
+
     // checking if at least one input is valid
     at_least_one_valid = 0;
     foreach(commit_instr_i[i]) begin
@@ -181,8 +224,34 @@ always_comb begin
         uop_entry_i[i].compressed = commit_instr_i.is_compressed;
         uop_entry_i[i].priv = priv_i;
     end
+    
     // assigning mux output
-    uop_entry_mux = uop_entry_o[cnt_val];
+    uop_entry_mux = uop_entry_o[mux_arb_val];
+
+    // counting the blocks to emit in one cycle
+    for (int i = 0; i < NRET; i++) begin
+        if ((uop_entry_o[i].itype > 0 && uop_entry_o[i].valid) || 
+            uop_entry_o[i].itype == 2) begin
+            n_blocks_i += 1;
+        end
+    end
+
+    // checking if all blocks are stored
+    if (demux_arb_val == n_blocks_o && !n_blocks_empty) begin
+        // setting outputs
+        for (int i = 0; i < n_blocks_o) begin
+            valid_o[i] = '1;
+            iretire_o[i] = iretire_q[i];
+            ilastsize_o[i] = ilastsize_q[i];
+            itype_o[i] = itype_q[i];
+            cause_o[i] = cause_q[i];
+            tval_o[i] = tval_q[i];
+            priv_o[i] = priv_q[i];
+            iaddr_o[i] = iaddr_q[i];
+        end
+        // popping the nblocks FIFO
+        n_blocks_pop = '1;
+    end
 end
 
 always_ff @( posedge clk_i, negedge rst_ni ) begin
@@ -190,6 +259,15 @@ always_ff @( posedge clk_i, negedge rst_ni ) begin
         is_taken_q <= '0;
         cause_q <= '0;
         tval_q <= '0;
+        for (int i = 0; i < N; i++) begin
+            iretire_q[i] <= '0;
+            ilastsize_q[i] <= '0;
+            itype_q[i] <= '0;
+            cause_q[i] <= '0;
+            tval_q[i] <= '0;
+            priv_q[i] <= '0;
+            iaddr_q[i] <= '0;
+        end
     end else begin
         if (resolved_branch_i.valid) begin
             is_taken_q <= is_taken_d;
@@ -198,7 +276,18 @@ always_ff @( posedge clk_i, negedge rst_ni ) begin
             cause_q <= cause_d;
             tval_q <= tval_d;
         end
+        if (valid_fsm) begin
+            iretire_q[demux_arb_val] <= iretire_d;
+            ilastsize_q[demux_arb_val] <= ilastsize_d;
+            itype_q[demux_arb_val] <= itype_d;
+            cause_q[demux_arb_val] <= cause_d;
+            tval_q[demux_arb_val] <= tval_d;
+            priv_q[demux_arb_val] <= priv_d;
+            iaddr_q[demux_arb_val] <= iaddr_d;
+        end
     end
+
+
 end
 
 endmodule
