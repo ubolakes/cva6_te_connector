c4e44b1fa18f0958de0ea206dfa815e9cfc50f73
Umberto Laghi <ul2509@gmail.com>
2024-10-21 15:50:52 +0200

    updated testbench

--- tb/tb_multiple_retirement.sv
+++ tb/tb_multiple_retirement.sv
@@ -6,7 +6,8 @@
 
 import mure_pkg::*;
 
-localparam NrRetiredInstr = 2;
+localparam NRET = 2;
+localparam N = 2;
 
 module tb_multiple_retirement();
 
@@ -14,56 +15,55 @@ module tb_multiple_retirement();
     logic reset;
 
     // inputs
-    logic [NrRetiredInstr-1:0][0:0]                 iretire_i;
-    logic [NrRetiredInstr-1:0][0:0]   ilastsize_i;
-    logic [NrRetiredInstr-1:0][ITYPE_LEN-1:0]       itype_i;
-    logic [CAUSE_LEN-1:0]                           cause_i;
-    logic [XLEN-1:0]                                tval_i;
-    logic [PRIV_LEN-1:0]                            priv_i;
-    logic [NrRetiredInstr-1:0][XLEN-1:0]            iaddr_i;
+    scoreboard_entry_t [NRET-1:0]   commit_instr_i;
+    bp_resolve_t                    resolved_branch_i;
+    exception_t                     exception_i;
+    logic                           interrupt_i;
+    logic [PRIV_LEN-1:0]            priv_lvl_i;
 
     // outputs
-    logic [0:0]                                 iretire_o;
-    logic                                       ilastsize_o;
-    logic [ITYPE_LEN-1:0]                       itype_o;
-    logic [CAUSE_LEN-1:0]                       cause_o;
-    logic [XLEN-1:0]                            tval_o;
-    logic [PRIV_LEN-1:0]                        priv_o;
-    logic [XLEN-1:0]                            iaddr_o;
+    logic [N-1:0]                   valid_o;
+    logic [N-1:0][IRETIRE_LEN-1:0]  iretire_o;
+    logic [N-1:0]                   ilastsize_o;
+    logic [N-1:0][ITYPE_LEN-1:0]    itype_o;
+    logic [N-1:0][CAUSE_LEN-1:0]    cause_o;
+    logic [N-1:0][XLEN-1:0]         tval_o;
+    logic [N-1:0][PRIV_LEN-1:0]     priv_o;
+    logic [N-1:0][XLEN-1:0]         iaddr_o;
 
     // testing only outputs
-    logic                                       expected_iretire;
-    logic                                       expected_ilastsize;
-    logic [ITYPE_LEN-1:0]                       expected_itype;
-    logic [CAUSE_LEN-1:0]                       expected_cause;
-    logic [XLEN-1:0]                            expected_tval;
-    logic [PRIV_LEN-1:0]                        expected_priv;
-    logic [XLEN-1:0]                            expected_iaddr;
+    logic [N-1:0]                   expected_valid;
+    logic [N-1:0][IRETIRE_LEN-1:0]  expected_iretire;
+    logic [N-1:0]                   expected_ilastsize;
+    logic [N-1:0][ITYPE_LEN-1:0]    expected_itype;
+    logic [N-1:0][CAUSE_LEN-1:0]    expected_cause;
+    logic [N-1:0][XLEN-1:0]         expected_tval;
+    logic [N-1:0][PRIV_LEN-1:0]     expected_priv;
+    logic [N-1:0][XLEN-1:0]         expected_iaddr;
 
     // iteration variable
     logic [31:0] i;
 
     // DUT instantiation
     multiple_retirement DUT(
-        .clk_i      (clk),
-        .rst_ni     (reset),
-        .iretire_i  (iretire_i),
-        .ilastsize_i(ilastsize_i),
-        .itype_i    (itype_i),
-        .cause_i    (cause_i),
-        .tval_i     (tval_i),
-        .priv_i     (priv_i),
-        .iaddr_i    (iaddr_i),
-        .iretire_o  (iretire_o),
-        .ilastsize_o(ilastsize_o),
-        .itype_o    (itype_o),
-        .cause_o    (cause_o),
-        .tval_o     (tval_o),
-        .priv_o     (priv_o),
-        .iaddr_o    (iaddr_o)
+        .clk_i            (clk),
+        .rst_ni           (reset),
+        .commit_instr_i   (commit_instr_i),
+        .resolved_branch_i(resolved_branch_i),
+        .exception_i      (exception_i),
+        .interrupt_i      (interrupt_i),
+        .priv_lvl_i       (priv_lvl_i),
+        .valid_o          (valid_o),
+        .iretire_o        (iretire_o),
+        .ilastsize_o      (ilastsize_o),
+        .itype_o          (itype_o),
+        .cause_o          (cause_o),
+        .tval_o           (tval_o),
+        .priv_o           (priv_o),
+        .iaddr_o          (iaddr_o)
     );
 
-    logic [188:0] test_vector[1000:0];
+    logic [342:0] test_vector[1000:0];
     //    length of line   # of lines
 
     initial begin // reading test vector
@@ -75,13 +75,12 @@ module tb_multiple_retirement();
 
     always @(posedge clk) begin // on posedge we get expected output
         {
-            iretire_i,
-            ilastsize_i,
-            itype_i,
-            cause_i,
-            tval_i,
-            priv_i,
-            iaddr_i,
+            commit_instr_i,
+            resolved_branch_i,
+            exception_i,
+            interrupt_i,
+            priv_lvl_i,
+            expected_valid,
             expected_iretire,
             expected_ilastsize,
             expected_itype,
@@ -93,6 +92,10 @@ module tb_multiple_retirement();
     end
 
     always @(negedge clk) begin// on negedge we compare the expected result with the actual one
+        // valid_o
+        if(expected_valid !== valid_o) begin
+            $display("Wrong valid: %b!=%b", expected_valid, valid_o);
+        end
         // iretire_o
         if(expected_iretire !== iretire_o) begin
             $display("Wrong iretire: %b!=%b", expected_iretire, iretire_o);
